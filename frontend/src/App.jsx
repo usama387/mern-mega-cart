92f1584b5442b2ad7004d7ab2df5a57089918bb0
usama387 <128306049+usama387@users.noreply.github.com>
2025-05-12 12:59:26 +0500

    Downgraded React version to 18.3.1 and tailwindcss to v3

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
+import { Button } from "./components/ui/button";
 
 const HomePage = () => {
-  return (
-    <div>
-      Home
+  return <div className="">
+    <Button>Press me</Button>
+    <h1 className="text-3xl font-bold underline text-blue-500 bg-blue-300">Hello world!</h1>;
     </div>
-  )
-}
+};
 
-export default HomePage
+export default HomePage;
