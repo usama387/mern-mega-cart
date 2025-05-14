86f95de1fe78260469026c030c99b0e92dd6b70b
usama387 <128306049+usama387@users.noreply.github.com>
2025-05-14 13:36:28 +0500

    Added HomePage route in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,18 @@
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./reusable/components/Navbar";
+import Home from "./pages/Home";
 
 const HomePage = () => {
-  return <div className="">
-    <Navbar />
+  return (
+    <div className="">
+      <Navbar />
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
     </div>
+  );
 };
 
 export default HomePage;
