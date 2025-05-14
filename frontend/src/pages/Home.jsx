c861602c5ff86f428e2b55eb3f2b62ba6cf1b7ef
usama387 <128306049+usama387@users.noreply.github.com>
2025-05-14 15:40:59 +0500

    Added new Collections component in Home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,14 @@
+import Collections from "@/reusable/components/Collections";
 import MainBanner from "@/reusable/components/MainBanner";
 import React from "react";
 
 const Home = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between">
-      <MainBanner />
+    <div className="">
+      <div className="flex flex-col items-center justify-between">
+        <MainBanner />
+      </div>
+      <Collections />
     </div>
   );
 };
---
+++ frontend/src/reusable/components/Collections.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { Card, CardContent, CardFooter } from "@/components/ui/card";
+import { categories } from "@/assets/assets";
+import { useAppContext } from "@/context/AppContext";
+
+const Collections = () => {
+  const { navigate } = useAppContext();
+
+  return (
+    <div className="px-6 md:px-16 lg:px-24 xl:px-32 py-8">
+      <h2 className="text-3xl md:text-4xl font-bold mb-6 bg-red-50 border border-red-100 w-max px-4 py-2 rounded-xl p-8 shadow-sm">
+        🌟 Explore Our <span className="text-red-600">Collections</span>
+      </h2>
+
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-6">
+        {categories.map((category, index) => (
+          <Card
+            key={index}
+            className="group cursor-pointer border-none shadow-lg hover:shadow-xl transition duration-300 ease-in-out rounded-2xl"
+            onClick={() => {
+              navigate(`/products/${category.path.toLowerCase()}`);
+              scrollTo(0, 0);
+            }}
+            style={{ backgroundColor: category.bgColor }}
+          >
+            <CardContent className="flex flex-col items-center justify-center py-6">
+              <img
+                src={category.image}
+                alt={category.text}
+                className="w-24 h-24 object-contain group-hover:scale-110 transition-transform duration-200"
+              />
+            </CardContent>
+            <CardFooter className="text-center">
+              <p className="text-md font-semibold text-gray-800 roup-hover:text-black">
+                {category.text}
+              </p>
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Collections;
