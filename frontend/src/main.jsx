56c03a2ab7fce39b9709d7bc82842cf5aa750cbc
usama387 <128306049+usama387@users.noreply.github.com>
2025-05-19 10:07:58 +0500

    Added toaster to show notifications

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,12 +2,15 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import { AppContextProvider } from "./context/AppContext";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import { Toaster } from "./components/ui/sonner";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <AppContextProvider>
+    <Provider store={store}>
       <App />
-    </AppContextProvider>
+      <Toaster />
+    </Provider>
   </BrowserRouter>
 );
