3ac6b291413643cd61f821c158bb581232ecafcf
usama387 <128306049+usama387@users.noreply.github.com>
2025-05-19 10:08:52 +0500

    Render login page when the state from redux slice is true

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,24 @@
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./reusable/components/Navbar";
 import Home from "./pages/Home";
+import Footer from "./reusable/components/Footer";
+import Login from "./reusable/components/Login";
+import { useSelector } from "react-redux";
 
 const HomePage = () => {
+  //state from redux that holds this state value either true or false
+  const { showUserLogin } = useSelector((store) => store?.loginSlice);
+
   return (
     <div className="">
       <Navbar />
+      {showUserLogin ? <Login /> : null}
       <div>
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
       </div>
+      <Footer />
     </div>
   );
 };
